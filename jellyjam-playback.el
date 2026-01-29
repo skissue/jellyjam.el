a1b9feccd84433bbd02792659e420d6a630fed7c
Ad <me@skissue.xyz>
2026-01-29 17:46:02 -0500

    refactor(playback): make jellyjam--mpv-send take list as args

--- jellyjam-playback.el
+++ jellyjam-playback.el
@@ -54,13 +54,13 @@ Return non-nil if there was an error."
 (defun jellyjam-play-track (id)
   "Play track ID with mpv."
   (let ((url (jellyjam--audio-url id)))
-    (jellyjam--mpv-send `["loadfile" ,url "replace"])
+    (jellyjam--mpv-send "loadfile" url "replace")
     (message "Playing track...")))
 
-(defun jellyjam--mpv-send (command)
-  "Send COMMAND list to mpv via IPC socket."
+(defun jellyjam--mpv-send (&rest args)
+  "Send ARGS to mpv via IPC socket."
   (jellyjam--ensure-mpv)
-  (let ((json (concat (json-serialize `(:command ,command)) "\n")))
+  (let ((json (concat (json-serialize `(:command ,(apply #'vector args))) "\n")))
     (with-temp-buffer
       (let ((proc (make-network-process
                    :name "jellyjam-mpv-ipc"
@@ -74,33 +74,33 @@ Return non-nil if there was an error."
         (ignore-errors (json-parse-buffer))))))
 
 (defun jellyjam-stop ()
-  "Stop mpv playback."
+  "Stop Jellyjam playback."
   (interactive)
-  (jellyjam--mpv-send '["stop"]))
+  (jellyjam--mpv-send "stop"))
 
 (defun jellyjam-pause ()
-  "Toggle mpv pause state."
+  "Toggle Jellyjam pause state."
   (interactive)
-  (jellyjam--mpv-send '["cycle" "pause"]))
+  (jellyjam--mpv-send "cycle" "pause"))
 
 (defun jellyjam-volume-set (volume)
-  "Set mpv volume to VOLUME (0-100)."
+  "Set Jellyjam volume to VOLUME (0-100)."
   (interactive "nVolume: ")
-  (jellyjam--mpv-send `["set_property" "volume" ,volume])
+  (jellyjam--mpv-send "set_property" "volume" volume)
   (message "Volume: %d" volume))
 
 (defun jellyjam-volume-up ()
-  "Increase mpv volume."
+  "Increase Jellyjam volume."
   (interactive)
-  (let* ((response (jellyjam--mpv-send '["get_property" "volume"]))
+  (let* ((response (jellyjam--mpv-send "get_property" "volume"))
          (current (gethash "data" response)))
     (when current
       (jellyjam-volume-set (min 100 (+ current jellyjam-volume-step))))))
 
 (defun jellyjam-volume-down ()
-  "Decrease mpv volume."
+  "Decrease Jellyjam volume."
   (interactive)
-  (let* ((response (jellyjam--mpv-send '["get_property" "volume"]))
+  (let* ((response (jellyjam--mpv-send "get_property" "volume"))
          (current (gethash "data" response)))
     (when current
       (jellyjam-volume-set (max 0 (- current jellyjam-volume-step))))))
