59e4d7d4427f037a2373dc58e062ad32c62c3ddf
Ad <me@skissue.xyz>
2026-01-30 23:07:20 -0500

    feat(playback): add queue tracking

--- jellyjam-playback.el
+++ jellyjam-playback.el
@@ -9,6 +9,34 @@
 (require 'jellyjam-api)
 (require 'jellyjam-mpv)
 
+(defvar jellyjam--queue nil
+  "List of track IDs in playback order.")
+
+(defvar jellyjam--queue-pos nil
+  "Current position in queue (0-indexed), or nil if empty.")
+
+(defvar jellyjam--queue-observer-registered nil
+  "Non-nil if playlist-playing-pos observer has been registered.")
+
+(defun jellyjam--queue-position-changed (pos)
+  "Handle playlist position change to POS from mpv."
+  (setq jellyjam--queue-pos pos))
+
+(defun jellyjam--ensure-queue-observer ()
+  "Ensure the queue position observer is registered."
+  (unless jellyjam--queue-observer-registered
+    (jellyjam-add-observer "playlist-playing-pos" #'jellyjam--queue-position-changed)
+    (setq jellyjam--queue-observer-registered t)))
+
+(defun jellyjam-current-track ()
+  "Return the ID of the currently playing track, or nil."
+  (when (and jellyjam--queue jellyjam--queue-pos)
+    (nth jellyjam--queue-pos jellyjam--queue)))
+
+(defun jellyjam-queue-length ()
+  "Return the number of tracks in the queue."
+  (length jellyjam--queue))
+
 (defcustom jellyjam-default-volume 100
   "Default volume passed to mpv (0-100)."
   :type 'integer
@@ -22,6 +50,9 @@
 (defun jellyjam-play-track (id &optional silent)
   "Play track ID with mpv.
 Show a message notifying the user unless SILENT is non-nil."
+  (jellyjam--ensure-queue-observer)
+  (setq jellyjam--queue (list id))
+  (setq jellyjam--queue-pos 0)
   (jellyjam--mpv-send "loadfile" (jellyjam--audio-url id) "replace")
   (jellyjam--mpv-send "set_property" "pause" :false)
   (unless silent
@@ -30,6 +61,7 @@ Show a message notifying the user unless SILENT is non-nil."
 (defun jellyjam-queue-track (id &optional silent)
   "Add track ID to mpv playlist.
 Show a message notifying the user unless SILENT is non-nil."
+  (setq jellyjam--queue (append jellyjam--queue (list id)))
   (jellyjam--mpv-send "loadfile" (jellyjam--audio-url id) "append")
   (unless silent
     (message "Queued track")))
@@ -46,6 +78,8 @@ Show a message notifying the user unless SILENT is non-nil."
   "Clear the Jellyjam playback queue.
 Show a message notifying the user unless SILENT is non-nil."
   (interactive)
+  (setq jellyjam--queue nil)
+  (setq jellyjam--queue-pos nil)
   (jellyjam--mpv-send "playlist-clear")
   ;; `playlist-clear' does not remove the current file.
   (jellyjam--mpv-send "playlist-remove" "current")
@@ -68,6 +102,8 @@ Show a message notifying the user unless SILENT is non-nil."
 (defun jellyjam-stop ()
   "Stop Jellyjam playback."
   (interactive)
+  (setq jellyjam--queue nil)
+  (setq jellyjam--queue-pos nil)
   (jellyjam--mpv-send "stop"))
 
 (defun jellyjam-pause ()
